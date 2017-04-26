7813a328bd8f9c086556b9ae31293b1572f2ad0f
Fabian Hoffmann <fabianhoffmann_@web.de>
2017-04-26 11:19:07 +0200

    add deleteStacks script

--- lambda/ci/pipeline.sh
+++ lambda/ci/pipeline.sh
@@ -6,31 +6,31 @@ if [ -z "${PROFILE}" ]; then
 fi
 
 if [ -z "${TEAM_NAME}" ]; then
-    echo "Please set environment variable API_GATEWAY_ID"
+    echo "Please set environment variable TEAM_NAME"
     exit 1
 fi
 
 # 1. Deploy resources
 bundle exec autostacker24 update --template lambda/cf-templates/chat-resources.yaml \
-    --stack lambda-chat-"${TEAM_NAME}-resources" \
+    --stack "lambda-chat-${TEAM_NAME}-resources" \
     --param TeamName="${TEAM_NAME}" \
     --profile "${PROFILE}"
 
 # 2. Deploy read lambda
 bundle exec autostacker24 update --template lambda/cf-templates/chat-read.yaml \
-    --stack lambda-chat-"${TEAM_NAME}-read" \
+    --stack "lambda-chat-${TEAM_NAME}-read" \
     --param TeamName="${TEAM_NAME}" \
     --profile "${PROFILE}"
 
 # 3. Deploy write lambda
 bundle exec autostacker24 update --template lambda/cf-templates/chat-write.yaml \
-    --stack lambda-chat-"${TEAM_NAME}-write" \
+    --stack "lambda-chat-${TEAM_NAME}-write" \
     --param TeamName="${TEAM_NAME}" \
     --profile "${PROFILE}"
 
 # 4. Deploy api
 bundle exec autostacker24 update --template lambda/cf-templates/chat-api.yaml \
-    --stack lambda-chat-"${TEAM_NAME}-api" \
+    --stack "lambda-chat-${TEAM_NAME}-api" \
     --param TeamName="${TEAM_NAME}" \
     --profile "${PROFILE}"
 
