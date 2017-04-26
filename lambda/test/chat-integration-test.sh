64c933cf0e22fe0e1f8a551d504ee7c125cd9e74
Fabian Hoffmann <fabianhoffmann_@web.de>
2017-04-26 10:24:22 +0200

    add initial integration test

---
+++ lambda/test/chat-integration-test.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+if [ -z "${API_GATEWAY_ID}" ]; then
+    echo "Please set environment variable API_GATEWAY_ID"
+    exit 1
+fi
+
+echo -e "Sending message .."
+curl -XPOST \
+    -d '{"username":"testuser","message": "test message"}' \
+    "https://${API_GATEWAY_ID}.execute-api.eu-central-1.amazonaws.com/LATEST/chat"
+success=$?
+if [ ${success} -eq 0 ]; then
+    echo -e "\nSend message successfully .."
+else
+    echo -e "\nSending message failed .."
+    exit 2
+fi
+
+echo -e "\nReading messages .."
+curl -s "https://${API_GATEWAY_ID}.execute-api.eu-central-1.amazonaws.com/LATEST/chat"
+success=$?
+if [ ${success} -eq 0 ]; then
+    echo -e "\nRead messages successfully .."
+else
+    echo -e "\nReading messages failed .."
+    exit 3
+fi
