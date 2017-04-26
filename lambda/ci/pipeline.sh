60d88675f316338a22a7ce0076b23d649508d31b
Fabian Hoffmann <fabianhoffmann_@web.de>
2017-04-26 11:13:56 +0200

    separate lambda functions and api

--- lambda/ci/pipeline.sh
+++ lambda/ci/pipeline.sh
@@ -10,9 +10,27 @@ if [ -z "${TEAM_NAME}" ]; then
     exit 1
 fi
 
-# 1. Deploy app
-bundle exec autostacker24 update --template lambda/cf-templates/chat.yaml \
-    --stack lambda-chat-"${TEAM_NAME}" \
+# 1. Deploy resources
+bundle exec autostacker24 update --template lambda/cf-templates/chat-resources.yaml \
+    --stack lambda-chat-"${TEAM_NAME}-resources" \
+    --param TeamName="${TEAM_NAME}" \
+    --profile "${PROFILE}"
+
+# 2. Deploy read lambda
+bundle exec autostacker24 update --template lambda/cf-templates/chat-read.yaml \
+    --stack lambda-chat-"${TEAM_NAME}-read" \
+    --param TeamName="${TEAM_NAME}" \
+    --profile "${PROFILE}"
+
+# 3. Deploy write lambda
+bundle exec autostacker24 update --template lambda/cf-templates/chat-write.yaml \
+    --stack lambda-chat-"${TEAM_NAME}-write" \
+    --param TeamName="${TEAM_NAME}" \
+    --profile "${PROFILE}"
+
+# 4. Deploy api
+bundle exec autostacker24 update --template lambda/cf-templates/chat-api.yaml \
+    --stack lambda-chat-"${TEAM_NAME}-api" \
     --param TeamName="${TEAM_NAME}" \
     --profile "${PROFILE}"
 
