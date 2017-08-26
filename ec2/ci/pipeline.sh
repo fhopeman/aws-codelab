6b97dd87bebedceeaf126728d2e1975d7cd30bdd
Matthias Splieth <matthias@splieth.com>
2017-08-26 13:45:54 +0200

    Simplify usage of GetAZs

--- ec2/ci/pipeline.sh
+++ ec2/ci/pipeline.sh
@@ -12,12 +12,15 @@ if [ -z "${TEAM_NAME}" ]; then
     exit 1
 fi
 
+REGION=${REGION:-"eu-central-1"}
+
 bundle check || bundle install
 
 # 1. Deploy vpc
-bundle exec autostacker24 update --template ec2/cf-templates/vpc.yaml \
+bundle exec autostacker24 update --template 4cheaters/ec2/cf-templates/vpc.yaml \
     --params ec2/properties/vpc.yaml \
     --stack "vpc-${TEAM_NAME}" \
+    --region "${REGION}" \
     --profile "${PROFILE}"
 
 # 2. Deploy debug security group
@@ -25,12 +28,14 @@ bundle exec autostacker24 update --template ec2/cf-templates/vpc.yaml \
 #    --stack "vpc-${TEAM_NAME}-debug-sg" \
 #    --param VPCStackName="vpc-${TEAM_NAME}" \
 #    --param TeamName="${TEAM_NAME}" \
+#    --region "${REGION}" \
 #    --profile "${PROFILE}"
 
 # 3. Deploy yocto
 #bundle exec autostacker24 update --template ec2/cf-templates/vpc-yocto.yaml \
 #    --params ec2/properties/yocto.yaml \
 #    --stack "vpc-${TEAM_NAME}-yocto" \
+#    --region "${REGION}" \
 #    --profile "${PROFILE}"
 
 # 4. Run integration tests if YOCTO_URL present
