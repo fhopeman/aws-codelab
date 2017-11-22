e69162aa6b61b6ef996b2411eaedffb8845f05a2
Marco Hutzsch <marco.hutzsch@otto.de>
2017-11-22 11:48:14 +0100

    fix security group to have access to the internet

--- terraform/yocto.tf
+++ terraform/yocto.tf
@@ -9,6 +9,13 @@ resource "aws_security_group" "httptraffic" {
     cidr_blocks = [
       "0.0.0.0/0"]
   }
+
+  egress {
+    from_port = 0
+    protocol = "-1"
+    to_port = 0
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
 
 data "template_file" "userdata" {
